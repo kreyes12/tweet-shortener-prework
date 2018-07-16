de07188d65668222b25dc7d31a9d5e3f6049a4f0
Karla R <karla.y.reyes12@gmail.com>
2018-07-16 05:57:00 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,5 @@
 def dictionary 
-dictionary = {"hello" => "hi", "to, two, too" => 2, "for, four" => 4, "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
+{"hello" => "hi", "to, two, too" => 2, "for, four" => 4, "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
 end
 
 def word_substituter(tweet)
